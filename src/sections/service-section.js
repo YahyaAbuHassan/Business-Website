6e603ee1d4cd7a911c93a35932186779ae2a84b7
YahyaAbuHassan <yehea20011@gmail.com>
2022-04-14 23:16:35 +1400

    first commit

--- src/sections/service-section.js
+++ src/sections/service-section.js
@@ -90,7 +90,7 @@ export default function ServiceSection() {
       {/* <ModalVideo 
       channel="youtube"
       isOpen={videoOpen}
-      videoId="iGBERMGMIvc"
+      videoId=""
       onClose={()=> setVideoOpen(false)}
 
       /> */}
